254293a4c8511c38c1ad28dff0ec412547c33af0
Steve <stevewu24@gmail.com>
2024-05-02 21:27:29 -0700

    updated studi2

--- studio2/script.js
+++ studio2/script.js
@@ -1,57 +1,50 @@
 (() => {
   'use strict';
-  const container = document.querySelector('.container');
+  const container = document.querySelector('.progress-container');
   let fetchedData;
 
   // Read data file 
   fetch('data.json')
-  .then(response => {
-    if (!response.ok) throw new Error('Network response was not ok');
-    return response.json(); // Parse JSON response
-  })
-  .then(data => {
-    fetchedData = data; // Assign fetched data to fetchedData variable
-    console.log(fetchedData);
-    // Initialize loop to create progress bars
-    for (let i = 1; i < fetchedData.length; i++) {
-      const progressBar = document.createElement('div');
-      progressBar.innerHTML = `<div class="progress-container"><span>${fetchedData[i].time}</span></div>`;
-      container.appendChild(progressBar);
-    }
-  })
-  .catch(error => {
-    console.error('There was a problem fetching the data:', error);
-  });
+    .then(response => {
+      if (!response.ok) throw new Error('Network response was not ok');
+      return response.json(); // Parse JSON response
+    })
+    .then(data => {
+      fetchedData = data; // Assign fetched data to fetchedData variable
 
-  // Initialize loop to create progress bars
-/*   for (let i = 1; i <= 5; i++) {
-    const progressBar = document.createElement('div');
-    progressBar.innerHTML = `<h2>Steps ${i}</h2><div class="progress-container"><div class="progress-bar"></div></div>`;
-    container.appendChild(progressBar);
-  } */
-  
-  const progressBars = document.querySelectorAll('.progress-bar'); // All progress bars DOM
+      // Initialize loop to create progress bars
+      for (let i = 0; i < fetchedData.length; i++) {
+        const progressBar = document.createElement('div');
+        progressBar.className = 'progress-item';
+        progressBar.setAttribute('id', fetchedData[i].id);
+        progressBar.innerHTML = `
+          <span>${fetchedData[i].time}</span>
+        `;
+        container.appendChild(progressBar);
+      }
 
-  // Function to animate the progress bars
-  const animateProgressBars = () => {
-    const endTime = 100; // End time for animation
-    const timeInterval = 15; // Time interval for animation speed
+      // Add event listener to each .progress-item
+      const progressItem = document.querySelectorAll('.progress-item');
+      const box = document.getElementById('box');
+      
+      progressItem.forEach(elem => {
+        let timeMode = true;
 
-    progressBars.forEach((progressBar) => {
-      let width = 1;
-      const animateFrame = () => {
-        if (width >= endTime) {
-          clearInterval(id);
-        } else {
-          width++;
-          progressBar.style.width = width + '%';
-        }
-      };
+        elem.addEventListener('click', () => {
+          const elemId = elem.id;
 
-      let id = setInterval(animateFrame, timeInterval);
+          const foundData = fetchedData.find(item => item.id == elemId);
+          const { miles, steps, time } = foundData; // Destructure the data from object
+
+          box.innerHTML = `
+              <span> Time: <b>${time}</b></span>
+              <span> Miles: <b>${miles}</b></span>
+              <span> Steps taken: <b>${steps}</b></span>
+            `;
+        });
+      });
+    })
+    .catch(error => {
+      console.error('There was a problem fetching the data:', error);
     });
-  };
-
-  // Call the function to animate the progress bars
-  animateProgressBars();
 })();
