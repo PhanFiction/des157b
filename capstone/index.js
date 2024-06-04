233b64bde85306ed4a5b5ee3718dc21784bd1a84
Steve <stevewu24@gmail.com>
2024-06-04 03:36:18 -0700

    added textbox, assets, and sections to capstone

--- capstone/index.js
+++ capstone/index.js
@@ -36,98 +36,195 @@
         }, 0);
   }) */
 
-  let text = gsap.timeline();
-  ScrollTrigger.create({
-    animation: text,
-    trigger: "#scene1",
-    start: "top",
-    end: "bottom center",
-    scrub: true,
-    markers: false,
-  });
-
-  gsap.from('#text1', {
-    x: -350,
-    y: 0,
-  })
-
-  gsap.from('#text2', {
-    x: 350,
-    y: 0,
-  })
-
-  gsap.from('#text3', {
-    x: 350,
-    y: 300,
-  })
-
-  gsap.from('#text4', {
-    x: -350,
-    y: 300,
-  })
-
-  text.to("#text1", { 
-    x: 350, 
-    y: 50, 
-  }, 0.5)
-
-  text.to("#text2", { 
-    x: -350,
-    y: 50,
-  }, 0.5)
-
-  text.to("#text3", { 
-    x: 100, 
-    y: 50, 
-  }, 0.5)
-
-  text.to("#text4", { 
-    x: -100,
-    y: 50,
-  }, 0.5)
-
-  ScrollTrigger.create({
-    animation: text,
-    trigger: "#scene2",
-    start: "top",
-    end: "bottom center",
-    scrub: true,
-    markers: false,
-  });
-
+  // Scene 1
   let scene1 = gsap.timeline();
   ScrollTrigger.create({
     animation: scene1,
     trigger: "#scene1",
     start: "top top",
-    end: "+=1000",
+    end: 'bottom center',
     scrub: true,
     pin: true,
     pinSpacing: true,
   });
+
+  // Scene 2
+  let sceneTwoText = gsap.timeline();
+  ScrollTrigger.create({
+    animation: sceneTwoText,
+    trigger: ".text-box",
+    start: "top",
+    end: "bottom",
+    scrub: false,
+    markers: false,
+    toggleActions: 'play onLeave onEnterBack onLeaveBack',
+  });
+
+  sceneTwoText.from('#text1', {
+    opacity: 0,
+    duration: 3,
+  })
+
+  sceneTwoText.to('#text1', {
+    opacity: 1,
+    duration: 3,
+  })
+  
+  sceneTwoText.from('#text2', {
+    opacity: 0,
+    duration: 9,
+    delay: 1000 * 5000,
+  })
+  
+  sceneTwoText.to('#text2', {
+    opacity: 1,
+    duration: 9,
+  })
 
   let scene2 = gsap.timeline();
   ScrollTrigger.create({
     animation: scene2,
     trigger: "#scene2",
     start: "top top",
-    end: "+=1000",
+    end: "+=700",
     scrub: true,
     pin: true,
     pinSpacing: true,
   });
+
+  ScrollTrigger.create({
+    animation: sceneTwoText,
+    trigger: "#scene2",
+    start: "top",
+    end: "bottom center",
+    scrub: true,
+    markers: false,
+  });
+
+
+  // Scene 3
+  let sceneThreeText = gsap.timeline();
+  ScrollTrigger.create({
+    animation: sceneThreeText,
+    trigger: ".text-box",
+    start: "top",
+    end: "bottom center",
+    scrub: false,
+    markers: false,
+    toggleActions: 'play onLeave onEnterBack onLeaveBack',
+  });
+
+  sceneThreeText.fromTo('#text3', 
+    {
+      opacity: 0,
+      duration: 3,
+      delay: 0.5,
+    },
+    {
+      opacity: 1,
+      duration: 3,
+    }
+  ).set('#text3', {
+    display: 'none',
+    duration: 3,
+  })
+
+  sceneThreeText.fromTo('#text4', 
+    {
+      opacity: 0,
+      duration: 3,
+      delay: 3,
+      display: 'none',
+    },
+    {
+      opacity: 1,
+      duration: 3,
+      display: 'block'
+    }
+  )
 
   let scene3 = gsap.timeline();
   ScrollTrigger.create({
     animation: scene3,
     trigger: "#scene3",
     start: "top top",
-    end: "bottom center",
+    end: "+=700",
     scrub: true,
-    pin: false,
-    pinSpacing: false,
+    pin: true,
+    pinSpacing: true,
   });
 
+  ScrollTrigger.create({
+    animation: sceneThreeText,
+    trigger: "#scene3",
+    start: "top",
+    end: "bottom center",
+    scrub: true,
+    markers: false,
+  });
+
+
+  // Scene 4
+  let sceneFourText = gsap.timeline();
+  ScrollTrigger.create({
+    animation: sceneFourText,
+    trigger: ".text-box",
+    start: "top",
+    end: "bottom center",
+    scrub: false,
+    markers: false,
+    toggleActions: 'play onLeave onEnterBack onLeaveBack',
+  });
+
+  sceneFourText.fromTo('#text5', 
+    {
+      opacity: 0,
+      duration: 3,
+      delay: 0.5,
+    },
+    {
+      opacity: 1,
+      duration: 3,
+    }
+  ).set('#text5', {
+    display: 'none',
+    duration: 3,
+  });
+
+  sceneFourText.fromTo('#text6', 
+    {
+      opacity: 0,
+      duration: 3,
+      delay: 3,
+      display: 'none',
+    },
+    {
+      opacity: 1,
+      x: 100,
+      duration: 3,
+      display: 'block'
+    }
+  );
+
+  let scene4 = gsap.timeline();
+  ScrollTrigger.create({
+    animation: scene4,
+    trigger: "#scene4",
+    start: "top top",
+    end: "+=700",
+    scrub: true,
+    pin: true,
+    pinSpacing: true,
+  });
+
+  ScrollTrigger.create({
+    animation: sceneFourText,
+    trigger: "#scene4",
+    start: "top",
+    end: "bottom center",
+    scrub: true,
+    markers: false,
+  });
 
   const lenis = new Lenis();
 
@@ -136,19 +233,4 @@
     requestAnimationFrame(raf);
   }
   requestAnimationFrame(raf);
-
-  
-/*   var scene = document.getElementById('scene');
-  var scene2 = document.getElementById('scene2');
-  var scene3 = document.getElementById('scene3');
-
-  var parallaxInstance;
-
-  function newParallaxScene(scene) {
-    parallaxInstance = new Parallax(scene);
-  }
-
-  newParallaxScene(scene);
-  newParallaxScene(scene2);
-  newParallaxScene(scene3); */
 })();
